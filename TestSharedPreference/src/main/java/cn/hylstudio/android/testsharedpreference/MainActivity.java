096b331a5e73cdbec5ec9c90069ce4c8dff9a76d
HuangYongliang <kuaile2911@126.com>
2016-09-02 10:33:58 +0800

    test SharedPreference

---
+++ TestSharedPreference/src/main/java/cn/hylstudio/android/testsharedpreference/MainActivity.java
@@ -0,0 +1,51 @@
+/**
+ * created by HYL
+ * 2016.9.2
+ */
+package cn.hylstudio.android.testsharedpreference;
+
+import android.app.Activity;
+import android.content.SharedPreferences;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private SharedPreferences s;
+    public static final String KEY_NAME = "name";
+    public static final String KEY_TEST = "test";
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button btn_read = (Button) findViewById(R.id.btn_read);
+        Button btn_write = (Button) findViewById(R.id.btn_write);
+
+        s = getSharedPreferences("test", Activity.MODE_PRIVATE);
+        btn_read.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String name = s.getString(KEY_NAME, "default name");
+                String test = s.getString(KEY_TEST, "default test");
+                Toast.makeText(MainActivity.this, name + ":" + test, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        btn_write.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                TextView t = (TextView) findViewById(R.id.text_test);
+                SharedPreferences.Editor e = s.edit();
+                e.putString(KEY_NAME, "hyl");
+                e.putString(KEY_TEST, t.getText() + "");
+                e.apply();
+                e.commit();
+            }
+        });
+    }
+}
