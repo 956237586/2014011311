e60339675cc7178db1d1b9a69de6b827333eaf02
HuangYongliang <kuaile2911@126.com>
2016-09-07 10:34:40 +0800

    exercise 30

--- TestView/src/main/java/cn/hylstudio/android/testview/MainActivity.java
+++ TestView/src/main/java/cn/hylstudio/android/testview/MainActivity.java
@@ -3,6 +3,10 @@ package cn.hylstudio.android.testview;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+/**
+ * 30：编程项目-[上机实践]简单图形绘制-具体内容
+ * created by hyl on 2016/9/7.
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
